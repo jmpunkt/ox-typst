21baea9710d0a7fa182f3b317e29b5b99bb9c193
Jonas Meurer <jmpunkt@outlook.com>
2024-10-31 18:40:32 +0100

    feat: add dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables            -*- no-byte-compile: t; -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode . ((indent-tabs-mode . nil)
+                     (tab-width . 8)
+                     (fill-column . 90)
+                     (sentence-end-double-space . t))))
