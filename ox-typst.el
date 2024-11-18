72304de5e9dd5d08530c46c80e0ec3b616976e52
Jonas Meurer <jmpunkt@outlook.com>
2024-11-18 22:05:10 +0100

    doc: add missing doc for interactive function

--- ox-typst.el
+++ ox-typst.el
@@ -712,6 +712,33 @@ start range of the timestamp is extracted."
 ;; Commands
 (defun org-typst-export-as-typst
     (&optional async subtreep visible-only body-only ext-plist)
+  "Export current buffer as a Typst buffer.
+
+If narrowing is active in the current buffer, only export its
+narrowed part.
+
+If a region is active, export that region.
+
+A non-nil optional argument ASYNC means the process should happen
+asynchronously.  The resulting buffer should be accessible
+through the `org-export-stack' interface.
+
+When optional argument SUBTREEP is non-nil, export the sub-tree
+at point, extracting information from the headline properties
+first.
+
+When optional argument VISIBLE-ONLY is non-nil, don't export
+contents of hidden elements.
+
+BODY-ONLY currently has no effect.  The entire buffer is always exported.
+
+EXT-PLIST, when provided, is a property list with external
+parameters overriding Org default settings, but still inferior to
+file-local settings.
+
+Export is done in a buffer named \"*Org Typst Export*\", which will be displayed
+when `org-export-show-temporary-export-buffer' is non-nil.  The resulting buffer
+will use the major mode specified by `org-typst-export-buffer-major-mode'."
   (interactive)
   (org-export-to-buffer 'typst org-typst-export-buffer-name
                         async subtreep visible-only body-only ext-plist
